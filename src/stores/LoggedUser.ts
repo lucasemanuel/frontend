69f92f582f060a4c0bb5720102d2f37503475575
Lucas Emanuel <lucaxemanuel@gmail.com>
2025-07-06 16:58:41 -0300

    chore: configurando pinia para armazenar dados do usuário

---
+++ src/stores/LoggedUser.ts
@@ -0,0 +1,33 @@
+import { defineStore } from "pinia";
+import type { User } from "@/stores/User";
+
+export const useLoggedUserStore = defineStore("loggedUser", {
+  state: () => ({
+    user: {
+      name: "",
+      hasAdminPermissions: false,
+      isLogged: false,
+    },
+  }),
+  actions: {
+    setUser(user: User) {
+      this.user.name = user.name;
+      this.user.hasAdminPermissions = user.hasAdminPermissions;
+      this.user.isLogged = user.isLogged;
+
+    },
+    clearUser() {
+      this.user.name = "";
+      this.user.hasAdminPermissions = false;
+      this.user.isLogged = false;
+    }
+  },
+  getters: {
+    name: (state) => {
+      return state.user.name;
+    },
+    hasAdminPermissions: (state) => {
+      return state.user.hasAdminPermissions;
+    }
+  },
+});
---
+++ src/stores/User.ts
@@ -0,0 +1,5 @@
+export type User = {
+    name: string;
+    hasAdminPermissions: boolean;
+    isLogged: boolean;
+};
